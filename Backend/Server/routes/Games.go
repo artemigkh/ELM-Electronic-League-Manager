17e97242ced4d1884929ca12af472cb53b12f744
Artem Kholodov <artemigkh@gmail.com>
2018-07-17 20:29:30 -0400

    Game Creation WIP

--- Backend/Server/routes/Games.go
+++ Backend/Server/routes/Games.go
@@ -1,6 +1,9 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 type GameInformation struct {
 	Team1ID int `json:"team1Id"`
@@ -8,6 +11,35 @@ type GameInformation struct {
 	GameTime int `json:"gameTime"`
 }
 
-func RegisterGameHandlers(g *gin.RouterGroup) {
+func createNewGame(ctx *gin.Context) {
+	//get parameters
+	var gameInfo GameInformation
+	err := ctx.ShouldBindJSON(&gameInfo)
+	if checkJsonErr(ctx, err) {
+		return
+	}
 
+	//must be logged in to create a game
+	userID, err := ElmSessions.AuthenticateAndGetUserID(ctx)
+	if checkErr(ctx, err) {
+		return
+	}
+	if userID == -1 {
+		ctx.JSON(http.StatusForbidden, gin.H{"error": "notLoggedIn"})
+		return
+	}
+
+	//must have an active game to create a team in it
+	leagueId, err := ElmSessions.GetActiveLeague(ctx)
+	if checkErr(ctx, err) {
+		return
+	}
+	if leagueId == -1 {
+		ctx.JSON(http.StatusForbidden, gin.H{"error": "noActiveLeague"})
+		return
+	}
+}
+
+func RegisterGameHandlers(g *gin.RouterGroup) {
+	g.POST("/", createNewGame)
 }

3171ab3443c92a4f772a1ec2b450b53b6abc9039
Artem Kholodov <artemigkh@gmail.com>
2018-12-23 22:53:01 -0500

    Updated frontend to display and update league dates

--- Frontend/src/app/manage/dates/manage-dates.ts
+++ Frontend/src/app/manage/dates/manage-dates.ts
@@ -1,4 +1,13 @@
 import {Component} from "@angular/core";
+import {MAT_MOMENT_DATE_FORMATS, MomentDateAdapter} from '@angular/material-moment-adapter';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+import {FormControl} from "@angular/forms";
+import {LeagueService} from "../../httpServices/leagues.service";
+import * as moment from "moment";
+import {Moment} from "moment";
+import {LeagueInformation} from "../../interfaces/LeagueInformation";
+import {ConfirmationComponent} from "../../shared/confirmation/confirmation-component";
+import {MatSnackBar} from "@angular/material";
 
 @Component({
     selector: 'app-manage-dates',
@@ -6,5 +15,50 @@ import {Component} from "@angular/core";
     styleUrls: ['./manage-dates.scss'],
 })
 export class ManageDatesComponent {
+    constructor(public confirmation: MatSnackBar, private leagueService: LeagueService) {
+        this.signupStart = new FormControl();
+        this.signupEnd = new FormControl();
+        this.leagueStart = new FormControl();
+        this.leagueEnd = new FormControl();
+        this.leagueInformation = null;
+        this.leagueService.getLeagueInformation().subscribe(
+            (next: LeagueInformation) => {
+                this.leagueInformation = next;
+                this.signupStart = new FormControl(moment.unix(next.signupStart));
+                this.signupEnd = new FormControl(moment.unix(next.signupEnd));
+                this.leagueStart = new FormControl(moment.unix(next.leagueStart));
+                this.leagueEnd = new FormControl(moment.unix(next.leagueEnd));
+            }, error => {
+                console.log(error);
+            }
+        )
+    }
 
+    onUpdate(): void {
+        this.leagueInformation.signupStart = this.signupStart.value.unix();
+        this.leagueInformation.signupEnd = this.signupEnd.value.unix();
+        this.leagueInformation.leagueStart = this.leagueStart.value.unix();
+        this.leagueInformation.leagueEnd = this.leagueEnd.value.unix();
+
+        this.leagueService.updateLeagueInformation(this.leagueInformation).subscribe(
+            next => {
+                this.confirmation.openFromComponent(ConfirmationComponent, {
+                    duration: 1250,
+                    panelClass: ['blue-snackbar'],
+                    data: {
+                        message: "League Dates Successfully Updated"
+                    }
+                });
+            }, error => {
+                console.log(error);
+                this.confirmation.openFromComponent(ConfirmationComponent, {
+                    duration: 2000,
+                    panelClass: ['red-snackbar'],
+                    data: {
+                        message: "Update Failed"
+                    }
+                });
+            }
+        );
+    }
 }

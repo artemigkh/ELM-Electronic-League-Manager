c10249afed518051f84a69a14b591a3ce2bca33c
Artem Kholodov <artemigkh@gmail.com>
2019-01-02 17:44:50 -0500

    Implemented checking permissions to view manage league and manage teams tabs

--- Frontend/src/app/manage/manage.ts
+++ Frontend/src/app/manage/manage.ts
@@ -1,5 +1,8 @@
 import {Component} from "@angular/core";
 import {LeagueService} from "../httpServices/leagues.service";
+import {UserService} from "../httpServices/user.service";
+import {TeamPermissions, UserPermissions} from "../httpServices/api-return-schemas/permissions";
+import {isUndefined} from "util";
 
 @Component({
     selector: 'app-manage',
@@ -7,5 +10,62 @@ import {LeagueService} from "../httpServices/leagues.service";
     styleUrls: ['./manage.scss'],
 })
 export class ManageComponent {
+    permissions: UserPermissions;
+    constructor(private userService: UserService) {
+        this.userService.getUserPermissions().subscribe(
+            (next: UserPermissions) => {
+                console.log(next);
+                this.permissions = next;
+            }, error => {
+                console.log(error);
+            }
+        );
+    }
 
+    displayLeagueControls(): boolean {
+        if(isUndefined(this.permissions)) {
+            return false;
+        } else {
+            return this.permissions.leaguePermissions.administrator;
+        }
+    }
+
+    displayTeamControls(): boolean {
+        if(isUndefined(this.permissions)) {
+            return false;
+        } else {
+            let display = this.permissions.leaguePermissions.administrator ||
+                this.permissions.leaguePermissions.editTeams;
+            this.permissions.teamPermissions.forEach((team: TeamPermissions) => {
+                display = display || team.administrator || team.information;
+            });
+            return display;
+        }
+    }
+
+    displayPlayerControls(): boolean {
+        if(isUndefined(this.permissions)) {
+            return false;
+        } else {
+            let display = this.permissions.leaguePermissions.administrator ||
+                this.permissions.leaguePermissions.editTeams;
+            this.permissions.teamPermissions.forEach((team: TeamPermissions) => {
+                display = display || team.administrator || team.players;
+            });
+            return display;
+        }
+    }
+
+    displayGameControls(): boolean {
+        if(isUndefined(this.permissions)) {
+            return false;
+        } else {
+            let display = this.permissions.leaguePermissions.administrator ||
+                this.permissions.leaguePermissions.editGames;
+            this.permissions.teamPermissions.forEach((team: TeamPermissions) => {
+                display = display || team.administrator || team.reportResults;
+            });
+            return display;
+        }
+    }
 }
--- Frontend/testingConfig.ts
+++ Frontend/testingConfig.ts
@@ -1,7 +1,9 @@
 export class TestingConfig {
     static testing = true; //in testing mode, app auto logs in, sets active league, and sets some behaviour to be constant
-    static email = "sophiabrown547@example.org"; //email to automatically log in with
-    static password = "neReMK95C6"; //password to automatically log in with
-    static leagueId = 4; //league id to automatically set active
+    static email = "madisonmoore253@test.com"; //email to automatically log in with
+    static password = "9t3tcg2Mb8"; //password to automatically log in with
+    static leagueId = 15; //league id to automatically set active
     static forceRegistrationPeriod = false; //if true, league will behave as if in registration mode
 }
+
+//sofiadavis033@example.com | wWsnZ7J2qw

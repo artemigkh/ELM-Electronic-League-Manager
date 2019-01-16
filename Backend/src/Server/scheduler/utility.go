b970f88b7c866e7ced76c503e6ff167a16c9dc55
Artem Kholodov <artemigkh@gmail.com>
2019-01-15 19:36:40 -0500

    Scheduling WIP

--- Backend/src/Server/scheduler/utility.go
+++ Backend/src/Server/scheduler/utility.go
@@ -14,6 +14,66 @@ func getRequiredGames(tournamentType int, teams []int) []sgame {
 	var requiredGames []sgame
 	numTeams := len(teams)
 	if tournamentType == RoundRobin {
+		//
+		//available := make([]int, len(teams))
+		//
+		//played := make([][]bool, len(teams))
+		//for i := range played {
+		//	played[i] = make([]bool, len(teams))
+		//}
+		//
+		//for _, i := range teams {
+		//	for _, j := range teams {
+		//		played[i][j] = i == j
+		//	}
+		//}
+		//
+		//for a := 0; a < len(teams) - 1; a++ {
+		//	for _, i := range teams {
+		//		available[i] = -1
+		//	}
+		//
+		//	for teamNumber := range teams {
+		//		availableCursor := 0
+		//		fmt.Printf("%v\n", available)
+		//		println(played[0][2])
+		//		for availableCursor < len(teams) {
+		//			if available[availableCursor] == -1 && math.Mod(float64(availableCursor), 2) == 0 {
+		//				available[availableCursor] = teamNumber
+		//				break
+		//			} else if available[availableCursor] == -1 && math.Mod(float64(availableCursor), 2) == 1 {
+		//				if !played[available[availableCursor-1]][teamNumber] {
+		//					available[availableCursor] = teamNumber
+		//					break
+		//				}
+		//			}
+		//			availableCursor++
+		//		}
+		//	}
+		//
+		//	for i := 0; i < len(teams); i += 2 {
+		//		requiredGames = append(requiredGames,
+		//			sgame{available[i], available[i+1]})
+		//		println("plays ", i)
+		//		played[available[i]][available[i+1]] = true
+		//		played[available[i+1]][available[i]] = true
+		//	}
+		//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 		curPos := 0
 		curJmp := 1
 		for numGames := 0; numGames < (numTeams*(numTeams-1))/2; numGames++ {
